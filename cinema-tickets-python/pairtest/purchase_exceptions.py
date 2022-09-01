0455b4efea2d9528552a3d7dc3626ed107fe1a2c
simon.smith <simon.smith1@engineering.digital.dwp.gov.uk>
2022-09-01 18:45:41 +0100

    chore(code): Initial push of Python coding template to github

---
+++ cinema-tickets-python/pairtest/purchase_exceptions.py
@@ -0,0 +1,4 @@
+
+class InvalidPurchaseException(Exception):
+
+    pass
---
+++ cinema-tickets-python/pairtest/ticket_service.py
@@ -0,0 +1,15 @@
+
+from purchase_exceptions import InvalidPurchaseException
+
+
+class TicketService:
+
+    """
+
+      purchase_tickets should be the only public method
+
+    """
+
+    def purchase_tickets(account_id=None, ticket_type_requests=[]):
+
+        raise InvalidPurchaseException
